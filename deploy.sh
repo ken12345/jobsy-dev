5b67f0188fd22858ff8647f3f997ed6dd1c25981
ken12345 <kengacilo@gmail.com>
2026-01-28 20:15:57 +0800

    put back changes

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,33 @@
-#!/bin/bash
+Hostinger
+
+db: u929529483_jobsy
+user: u929529483_kenjobsy
+password: 1nd3p3nd3ntJ0bs
+
+cpanel password: 1nd3p3nd3nT-prodigy
+
+server: https://developers.hostinger.com
+
+wGMUwkwNGJHv2ysFIHxKaITaYGii5ge2SDoSWO5q84f3002f
+
+
+curl -X GET "https://developers.hostinger.com/api/vps/v1/virtual-machines" \
+-H "Authorization: Bearer wGMUwkwNGJHv2ysFIHxKaITaYGii5ge2SDoSWO5q84f3002f" \
+-H "Content-Type: application/json"
+
+public ip: 72.61.119.52
+
+to connect: ssh root@72.61.119.52
+
+
+sudo usermod -a -G your_user www-data
+
+sudo chmod g+x /home/your_user
+
+
+
+
+
 set -e
 
 # Load NVM
@@ -40,4 +69,4 @@ chown -R www-data:www-data /var/www/jobsy
 chmod -R 755 /var/www/jobsy
 
 echo "Deployment finished successfully."
-#!/bin/bash
+
